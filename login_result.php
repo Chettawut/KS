0be2c58e2189f89c5c0aeee9f65c2c7d9b30769a
Chayapat Niropas <papae_peez02@hotmail.co.th>
2023-02-19 15:55:58 +0700

    19/02/2023

--- login_result.php
+++ login_result.php
@@ -9,7 +9,7 @@ if ( !isset($_POST['username'], $_POST['password']) ) {
 	exit('Please fill both the username and password fields!');
 }
 // Prepare our SQL, preparing the SQL statement will prevent SQL injection.
-if ($stmt = $conn->prepare('SELECT firstname,lastname,id,password,type,status FROM user WHERE username = ?')) {
+if ($stmt = $conn->prepare('SELECT firstname,lastname,code,password,type,status FROM user WHERE username = ?')) {
 	// Bind parameters (s = string, i = int, b = blob, etc), in our case the username is a string so we use "s"
 	$stmt->bind_param('s', $_POST['username']);
 	$stmt->execute();
@@ -20,7 +20,7 @@ if ($stmt = $conn->prepare('SELECT firstname,lastname,id,password,type,status FR
 }
 
 if ($stmt->num_rows > 0) {
-	$stmt->bind_result($firstname,$lastname,$id,$password,$type,$status);
+	$stmt->bind_result($firstname,$lastname,$code,$password,$type,$status);
 	$stmt->fetch();
 	// Account exists, now we verify the password.
 	// Note: remember to use password_hash in your registration file to store the hashed passwords.
@@ -29,7 +29,7 @@ if ($stmt->num_rows > 0) {
 			session_regenerate_id();
 			$_SESSION['loggedin'] = TRUE;
 			$_SESSION['name'] = $_POST['username'];
-			$_SESSION['id'] = $id;
+			$_SESSION['code'] = $code;
 			$_SESSION['firstname'] = $firstname;
 			$_SESSION['lastname'] = $lastname;
 		
